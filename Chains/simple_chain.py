99d9d3758377af466cfe923bdd2d355f25d82f22
imharshmishra87 <harsh87mishra@gmail.com>
2026-05-23 18:53:31 +0530

    Implemented a simple chain and it's working

---
+++ Chains/simple_chain.py
@@ -0,0 +1,29 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+llm_endpoint=HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V4-Pro",task="text generation")
+model=ChatHuggingFace(llm=llm_endpoint)
+
+
+template=PromptTemplate(
+    template="Generate 5 interesting facts about {topic}",
+    input_variables=["topic"]
+)
+
+parser=StrOutputParser()
+
+chain= template | model | parser
+
+
+print(chain.get_graph().draw_ascii())
+
+result=chain.invoke({"topic":"Harry Potter"})
+print(result)
+
+
+
+
